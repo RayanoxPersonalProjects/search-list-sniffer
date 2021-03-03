d94297f15ca8d99a843e38caf2e9d20839c1128f
Rayanox <rayane.benhmidane32@hotmail.fr>
2021-03-03 23:27:58 +0100

    Remove empty test

--- src/test/java/com/rb/searchlistsniffer/SearchListSnifferApplicationTests.java
+++ src/test/java/com/rb/searchlistsniffer/SearchListSnifferApplicationTests.java
@@ -3,10 +3,10 @@ package com.rb.searchlistsniffer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SearchListSnifferApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
